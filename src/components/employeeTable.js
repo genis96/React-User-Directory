72da86e5af0a4c2bfd2d477f79ac2ffc0213cdd8
Genis Bardales <genis96@yahoo.com>
2021-03-27 18:08:58 -0400

    edits

--- src/components/employeeTable.js
+++ src/components/employeeTable.js
@@ -1,2 +1,10 @@
 import React from "react";
 import "../../styles/employeeTable.css";
+
+function employeeTable() {
+    return (
+
+    );
+}
+
+export default employeeTable;
--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -1,3 +1,10 @@
 import React from "react";
 import "../../styles/jumbotron.css";
 
+function Jumbotron() {
+    return (
+
+    );
+}
+
+export default Jumbotron;
