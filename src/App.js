036f48ef7d81b60e2c685c95b93d69470ee942a6
Genis Bardales <genis96@yahoo.com>
2021-03-27 18:06:50 -0400

    working on structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import './App.css';
-import Header from './components/Header'
-import SearchBar from './components/SearchBar'
-import EmployeeTable from './components/EmployeeTable'
+import Jumbotron from './components/jumbotron'
+import SearchBar from './components/searchBar'
 
 
 function App() {
   return (
-    
+    <div>
+      <Jumbotron/>
+      <SearchBar/>
+    </div>
   );
 }
 
