9b5a12d4b11760e7e325c37045c774c654173bc7
Genis Bardales <genis96@yahoo.com>
2021-03-27 20:50:18 -0400

    componentDidMount function and state but need to add more.. first and last name orders

--- src/components/employeeTable.js
+++ src/components/employeeTable.js
@@ -6,16 +6,18 @@ import DateFormat from 'dateformat';
 
 class EmployeeTable extends Component {
     state = {
-
+        search: "",
+        results: []
     };
     // When the component mounts, get a list of names and update this.state.results
-    componentDidMount() {}
-
-
+    componentDidMount() {
+        API.searchApiEmployees()
+        .then(res => { this.setState({ results: res.data.results }) 
+        console.log(this.state.results)
+        }).catch(err => console.log(err));
+    }
 
     // handleInputChange function - for the input inside the search bar
-
-
 
     // handleInputChange function search for target.value
 
